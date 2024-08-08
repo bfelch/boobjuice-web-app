675577ebe7a214d2edbcb6190b405831a158c3d2
wugbor <felchbrandon@gmail.com>
2024-08-07 20:26:15 -0500

    fixing bad test case

--- tests/utils/test_date_utils.py
+++ tests/utils/test_date_utils.py
@@ -10,8 +10,8 @@ def test_datetime_from_timestamp_none():
 	assert dt is None
 
 def test_datetime_from_timestamp_invalid():
-	timestamp = '2024-08-07T17:36'
-	dt = date_utils.datetime_from_timestamp(timestamp, '%invalid format')
+	timestamp = '2024-08-32T17:36'
+	dt = date_utils.datetime_from_timestamp(timestamp)
 
 	assert dt is None
 
@@ -23,12 +23,6 @@ def test_datetime_from_timestamp_valid():
 
 def test_timestamp_from_datetime_none():
 	timestamp = date_utils.timestamp_from_datetime(None)
-
-	assert timestamp is None
-
-def test_timestamp_from_datetime_invalid():
-	dt = datetime(2024, 8, 7, 17, 36)
-	timestamp = date_utils.timestamp_from_datetime(dt, '%invalid format')
 
 	assert timestamp is None
 
